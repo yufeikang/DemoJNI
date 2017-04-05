1fc0431cc52e76a8661164f8411622c10a273be2
kang <kyf0722@gmail.com>
2017-04-05 16:28:47 +0800

    ndk14 support

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,4 +2,5 @@ APP_ABI := all
 NDK_TOOLCHAIN_VERSION := clang
 APP_SHORT_COMMANDS      := true
 APP_STL := stlport_static
+APP_ALLOW_MISSING_DEPS=true
 APP_CPPFLAGS := -std=gnu++11 -D__STDC_LIMIT_MACROS
